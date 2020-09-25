79a46d19004b0a199051147cb459b7d86d3da781
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-25 10:51:33 -0700

    Add script to run program in console

--- app.rb
+++ app.rb
@@ -1,2 +1,20 @@
 #!usr/bin/ruby
 require ('./lib/anagram')
+
+puts ""
+puts "*******************************************************"
+puts "* Anagrams, Antigrams, and Palindromes . . . Oh My!!! *"
+puts "*******************************************************"
+puts ""
+puts ">>> Please enter a word or sentence:"
+string1 = gets.chomp
+base_string = Anagram.new(string1) 
+puts ""
+puts ">>> Please enter a word or sentence to compare:"
+string2 = gets.chomp
+puts ""
+puts ">>> #{base_string.anagram_check(string2)}"
+puts ""
+puts ">>> . . . Goodbye!"
+puts ""
+ 
