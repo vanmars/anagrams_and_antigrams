b8bfc57aea3048e713ada288bf4fff8d46b52e19
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-25 13:55:51 -0700

    Refactor palindrome methods

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,66 +10,12 @@ class Anagram
       return "Error: You have entered at least one non-word. Try again."
     end
     if anagram?(string2)
-      return anagram_palindrome_matches(string2)
+      return "These words are anagrams!#{palindrome_results(string2)}"
     end
     if antigram?(string2)
-      return antigram_palindrome_matches(string2)
+      return "These words have no letter matches and are antigrams.#{palindrome_results(string2)}"
     end
-    return letter_matches(string2)
-  end
-
-
-  def anagram_palindrome_matches(string2)
-    string2_letters_only = string2.gsub(/[\s\W]/,"")
-    if palindrome?(@letters_only_string) && palindrome?(string2_letters_only)
-      return "These words are anagrams! (By the way . . . '#{@string}' and '#{string2}' are both palindromes!)"
-    elsif palindrome?(@letters_only_string)
-      return "These words are anagrams! (By the way . . . '#{@string}' is a palindrome!)"
-    elsif palindrome?(string2_letters_only)
-      return "These words are anagrams! (By the way . . . '#{string2}' is a palindrome!)"
-    else
-      return "These words are anagrams!"
-    end
-  end
-
-  def antigram_palindrome_matches(string2)
-    string2_letters_only = string2.gsub(/[\s\W]/,"")
-    if palindrome?(@letters_only_string) && palindrome?(string2_letters_only)
-      return "These words have no letter matches and are antigrams. (By the way . . . '#{@string}' and '#{string2}' are both palindromes!)"
-    elsif palindrome?(@letters_only_string)
-      return "These words have no letter matches and are antigrams. (By the way . . . '#{@string}' is a palindrome!)"
-    elsif palindrome?(string2_letters_only)
-      return "These words have no letter matches and are antigrams. (By the way . . . '#{string2}' is a palindrome!)"
-    else
-      return "These words have no letter matches and are antigrams."
-    end
-  end
-
-
-
-  def letter_matches(string2)
-    string2_letters_only = string2.gsub(/[\s\W]/,"")
-    result = []
-    @letters_only_string.each_char do |c|
-      if string2_letters_only.include?(c)
-        result.push(c)
-        string2_letters_only.sub!(c, "")
-      end
-    end
-    result_string = result.join(", ")
-    if palindrome?(@letters_only_string) && palindrome?(string2_letters_only)
-      return "These words aren't anagrams, but #{result.length} letters match: #{result_string}. (By the way . . . '#{@string}' and '#{string2}' are both palindromes!)"
-    elsif palindrome?(@letters_only_string)
-      return "These words aren't anagrams, but #{result.length} letters match: #{result_string}. (By the way . . . '#{@string}' is a palindrome!)"
-    elsif palindrome?(string2_letters_only)
-      return "These words aren't anagrams, but #{result.length} letters match: #{result_string}. (By the way . . . '#{string2}' is a palindrome!)"
-    else
-      return "These words aren't anagrams, but #{result.length} letters match: #{result_string}."
-    end
-  end
-
-  def palindrome?(input)
-    return input.downcase == input.downcase.reverse
+    return "#{letter_matches(string2)}#{palindrome_results(string2)}"
   end
 
   def input_contains_non_words?
@@ -88,6 +34,23 @@ class Anagram
       end
     end
     return false
+  end
+
+  def palindrome?(input)
+    return input.downcase == input.downcase.reverse
+  end
+
+  def palindrome_results(string2)
+    string2_letters_only = string2.gsub(/[\s\W]/,"")
+    if palindrome?(@letters_only_string) && palindrome?(string2_letters_only)
+      return " (By the way . . . '#{@string}' and '#{string2}' are both palindromes!)"
+    elsif palindrome?(@letters_only_string)
+      return " (By the way . . . '#{@string}' is a palindrome!)"
+    elsif palindrome?(string2_letters_only)
+      return " (By the way . . . '#{string2}' is a palindrome!)"
+    else
+      return
+    end
   end
 
   def anagram?(string2)
@@ -117,4 +80,17 @@ class Anagram
     return true
   end
 
+  def letter_matches(string2)
+    string2_letters_only = string2.gsub(/[\s\W]/,"")
+    result = []
+    @letters_only_string.each_char do |c|
+      if string2_letters_only.include?(c)
+        result.push(c)
+        string2_letters_only.sub!(c, "")
+      end
+    end
+    result_string = result.join(", ")
+    return "These words aren't anagrams, but #{result.length} letters match: #{result_string}."
+  end
+  
 end
