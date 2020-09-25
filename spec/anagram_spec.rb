2d0da42131f3e9e8323276e14557f95cc3d22f51
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-25 08:59:05 -0700

    Write and FAIL  another test for Spec 1 to see if words that are not anagrams are accounted for

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -8,4 +8,9 @@ describe ('Anagram#anagram_check') do
     expect(string.anagram_check("bury")).to(eq("These words are anagrams!"))
   end
 
+  it('correctly checks whether two words are anagrams') do
+    string = Anagram.new('ruby')
+    expect(string.anagram_check("burry")).to(eq("These words are not anagrams!"))
+  end
+
 end
