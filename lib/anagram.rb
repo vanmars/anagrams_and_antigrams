ab3bdef7811f93a3bb3f8e0615f684144fca8fdc
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-25 10:32:24 -0700

    Refactor by eliminating extra spacces and re-ordering methods for readability

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,6 +5,37 @@ class Anagram
     @letters_only_string = @string.gsub(/[\s\W]/, "")
   end
 
+  def anagram_check(string2)
+    if input_contains_non_words?() || comparison_contains_non_words?(string2)
+      return "Error: You have entered at least one non-word. Try again."
+    end
+    if anagram?(string2)
+      return "These words are anagrams!"
+    end
+    if antigram?(string2)
+      return "These words have no letter matches and are antigrams."
+    end
+   return "These words are not anagrams."
+  end
+
+  def input_contains_non_words?
+    @string.split(" ").each do |word|
+      if !word.downcase.match?(/[aeiou]/)
+        return true
+      end
+    end
+    return false
+  end
+
+  def comparison_contains_non_words?(string2)
+    string2.split(" ").each do |word|
+      if !word.downcase.match?(/[aeiou]/)
+        return true
+      end
+    end
+    return false
+  end
+
   def anagram?(string2)
     string2_letters_only = string2.gsub(/[\s\W]/,"")
     if @letters_only_string.length != string2_letters_only.length
@@ -30,41 +61,6 @@ class Anagram
       end
     end
     return true
-  end
-
-  def input_contains_non_words?
-    @string.split(" ").each do |word|
-      if !word.downcase.match?(/[aeiou]/)
-        return true
-      end
-    end
-    return false
-  end
-
-  def comparison_contains_non_words?(string2)
-    string2.split(" ").each do |word|
-      if !word.downcase.match?(/[aeiou]/)
-        return true
-      end
-    end
-    return false
-  end
-
-
-  def anagram_check(string2)
-    if comparison_contains_non_words?(string2) || input_contains_non_words?()
-      return "Error: You have entered at least one non-word. Try again."
-    end
-
-    if anagram?(string2)
-      return "These words are anagrams!"
-    end
-
-    if antigram?(string2)
-      return "These words have no letter matches and are antigrams."
-    end
-
-   return "These words are not anagrams."
   end
 
 end
