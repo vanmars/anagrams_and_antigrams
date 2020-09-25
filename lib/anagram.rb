05c31c550412813e0fc4a0d6df7b9f0b44ded8a1
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-25 09:50:17 -0700

    PASS test for Spec 4: Program checks for antigrams

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,16 +4,46 @@ class Anagram
     @string = string_input
   end
 
+  def anagram?(string2)
+    if @string.length != string2.length
+      return false
+    end
+    @string.each_char do |c|
+      if @string.downcase.count(c.downcase) == string2.downcase.count(c.downcase)
+        next
+      else 
+        return false
+      end
+    end
+    return true
+  end
+  
+  def antigram? (string2)
+    @string.each_char do |c|
+      if @string.downcase.count(c.downcase) != string2.downcase.count(c.downcase)
+        next
+      else 
+        return false
+      end
+    end
+    return true
+  end
+
+
   def anagram_check(string2)
     if !@string.downcase.match?(/[aeiou]/) || !string2.downcase.match?(/[aeiou]/)
       return "Error: You have entered a non-word. Try again."
     end
-    @string.each_char do |c|
-      if @string.downcase.count(c.downcase) != string2.downcase.count(c.downcase)
-        return "These words are not anagrams."
-      end
+
+    if anagram?(string2)
+      return "These words are anagrams!"
     end
-    return "These words are anagrams!"
+
+    if antigram?(string2)
+      return "These words have no letter matches and are antigrams."
+    end
+
+   return "These words are not anagrams."
   end
 
 end
