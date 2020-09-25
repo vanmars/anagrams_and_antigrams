17363c9ef2d4090fa5744009f6f5d18e5a9605f7
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-25 15:21:49 -0700

    Refactor not_word? method

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,8 +1,7 @@
 require ('dictionary_lookup')
 
 def not_word?(string)
-  string_array = string.split(" ")
-  string_array.each do |word|
+  string.split(" ").each do |word|
     word = word.gsub(/[\W]/, "")
     if DictionaryLookup::Base.define(word).count == 0
       return true
