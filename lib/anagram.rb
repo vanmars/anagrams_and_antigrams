0db3627cc50370b344ebb6388aa6416b09f3da38
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-25 09:11:05 -0700

    PASS test for Spec 3: Program does not include non-words as anagrams

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,6 +5,9 @@ class Anagram
   end
 
   def anagram_check(string2)
+    if !@string.downcase.match?(/[aeiou]/) || !string2.downcase.match?(/[aeiou]/)
+      return "Error: You have entered a non-word. Try again."
+    end
     @string.each_char do |c|
       if @string.downcase.count(c.downcase) != string2.downcase.count(c.downcase)
         return "These words are not anagrams."
