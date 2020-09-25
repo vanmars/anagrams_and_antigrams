1cdf1b07ff4cd9f13692361324f33d5e40fed02b
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-25 10:58:09 -0700

    Write and FAIL test for Spec 7: Retur how many letters match for non-anagrams

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,39 +3,44 @@ require ('anagram')
 
 describe ('Anagram#anagram_check') do
   
-  it('correctly checks whether two words are anagrams') do
+  it('Spec 1: Correctly checks whether two words are anagrams') do
     string = Anagram.new('ruby')
     expect(string.anagram_check("bury")).to(eq("These words are anagrams!"))
   end
 
-  it('correctly checks whether two words are not anagrams') do
+  it('Spec 1: Correctly checks whether two words are not anagrams') do
     string = Anagram.new('ruby')
     expect(string.anagram_check("burry")).to(eq("These words are not anagrams."))
   end
 
-  it('accounts for capitalized letters') do
+  it('spec 2: Accounts for capitalized letters') do
     string = Anagram.new('Tea')
     expect(string.anagram_check("Eat")).to(eq("These words are anagrams!"))
   end
 
-  it('does not include non-words in anagram test') do
+  it('Spec 3: Does not include non-words in anagram test') do
     string = Anagram.new('ctgc')
     expect(string.anagram_check("gcct")).to(eq("Error: You have entered at least one non-word. Try again."))
   end
 
-  it('checks for antigrams') do
+  it('Spec 4: Checks for antigrams') do
     string = Anagram.new('hi')
     expect(string.anagram_check("bye")).to(eq("These words have no letter matches and are antigrams."))
   end
 
-  it('works with spaces and punctuation') do
+  it('Spec 5: Works with spaces and punctuation') do
     string = Anagram.new('Tom Marvolo Riddle')
     expect(string.anagram_check("I Am Lord Voldemort")).to(eq("These words are anagrams!"))
   end
 
-  it('works with spaces and punctuation, but rejects non-words in a sentence') do
+  it('Spec 6: Works with spaces and punctuation, but rejects non-words in a sentence') do
     string = Anagram.new('Tzm Mqrvzlz Rbddle')
     expect(string.anagram_check("B Qm Lzrd Vzldemzrt")).to(eq("Error: You have entered at least one non-word. Try again."))
+  end
+
+  it('Spec 7: Returns how many letters are actual matches') do
+    string = Anagram.new('cat')
+    expect(string.anagram_check("batty")).to(eq("These words aren't anagrams, but 2 letters match: a, t."))
   end
 
 end
