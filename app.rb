07303f2a21e2b0054c4ece2366cb12d05c513044
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-25 16:28:45 -0700

    Enable user to re-run or quit program

--- app.rb
+++ app.rb
@@ -7,31 +7,40 @@ puts "*******************************************************"
 puts "* Anagrams, Antigrams, and Palindromes . . . Oh My!!! *"
 puts "*******************************************************"
 puts ""
-puts ">>> Please enter a word or sentence:"
 
-string1 = gets.chomp
-
-while not_word?(string1) do
-  puts ">>> Error: '#{string1}' contains a non-word. Please enter another word or sentence:"
+play = 'y'
+while play == 'y' do
+  puts ">>> Please enter a word or sentence:"
   string1 = gets.chomp
-end
 
-base_string = Anagram.new(string1) 
+  while not_word?(string1) do
+    puts ">>> Error: '#{string1}' contains a non-word. Please enter another word or sentence:"
+    string1 = gets.chomp
+  end
 
-puts ""
-puts ">>> Please enter a word or sentence to compare:"
+  base_string = Anagram.new(string1) 
 
-string2 = gets.chomp
+  puts ""
+  puts ">>> Please enter a word or sentence to compare:"
 
-while not_word?(string2) do
-  puts ">>> Error: '#{string2}' contains a non-word. Please enter another word or sentence:"
   string2 = gets.chomp
+
+  while not_word?(string2) do
+    puts ">>> Error: '#{string2}' contains a non-word. Please enter another word or sentence:"
+    string2 = gets.chomp
+  end
+
+  puts ""
+  puts ">>> #{base_string.anagram_check(string2)}"
+  puts ""
+
+  puts "******************************************"
+  puts "Would you like to enter another word (y/n)"
+  puts "******************************************"
+  play = gets.chomp.downcase
 end
 
 puts ""
-puts ">>> #{base_string.anagram_check(string2)}"
-puts ""
-
 puts "********"
 puts "Goodbye!"
 puts "********"
