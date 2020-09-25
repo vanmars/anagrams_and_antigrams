112c6d8c5ad59e13fda9198b9d32d0faec00a3fe
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-25 08:57:41 -0700

    PASS test for Spec 1: Programs checkes whether two words are anagrams

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,7 +4,13 @@ class Anagram
     @string = string_input
   end
 
-  def anagram_check
+  def anagram_check(string2)
+    @string.each_char do |c|
+      if @string.count(c) != string2.count(c)
+        return "These words are not anagrams."
+      end
+    end
+    return "These words are anagrams!"
   end
 
 end
