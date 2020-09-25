f045498e002566c78dd5c852d92a16a147e7a8e2
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-25 09:01:27 -0700

    Write and FAIL test for Spec 2: Program accounts for capitalized letters

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -8,9 +8,13 @@ describe ('Anagram#anagram_check') do
     expect(string.anagram_check("bury")).to(eq("These words are anagrams!"))
   end
 
-  it('correctly checks whether two words are anagrams') do
+  it('correctly checks whether two words are not anagrams') do
     string = Anagram.new('ruby')
     expect(string.anagram_check("burry")).to(eq("These words are not anagrams."))
   end
 
+  it('accounts for capitalized letters') do
+    string = Anagram.new('Tea')
+    expect(string.anagram_check("Eat")).to(eq("These words are anagrams!"))
+  end
 end
